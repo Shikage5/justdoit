fb024848436fdd89e2ea9e4605ef05a3165a6218
Salajeanu <gabriel.salajeanu@fujitsu.com>
2022-03-30 14:37:45 +0200

    HttpRequests

--- server/Controllers/DoItController.cs
+++ server/Controllers/DoItController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using JustDoIt.Models;
 using JustDoIt.Data;
+using JustDoIt.ViewModels;
+
 [ApiController]
 [Route("[controller]")]
 public class DoItController : ControllerBase
@@ -15,10 +17,60 @@ public class DoItController : ControllerBase
         _context = context;
     }
 
-    [HttpGet]
-    public IEnumerable<ToDoItem> GetToDos()
+    [HttpGet("get")]
+    public IActionResult GetToDos()
+    {
+        var todos = _context.ToDoItems.ToList()!;
+
+        return Ok(todos);
+
+    }
+    [HttpPost("new")]
+    public IActionResult PostToDo([FromBody] AddToDoVm addToDo)
     {
 
+        var todo = new ToDoItem();
+        todo.Name = addToDo.Name;
+        todo.Priority = addToDo.Priority;
+        todo.DueDate = addToDo.DueDate;
+        todo.DueTime = addToDo.DueTime;
+        todo.Status = addToDo.Status;
 
+        _context.ToDoItems.Add(todo);
+        _context.SaveChanges();
+
+        return Ok(todo);
+    }
+    [HttpPost("update")]
+    public IActionResult UpdateToDo([FromQuery] int Id, [FromBody] AddToDoVm updateToDo)
+    {
+        var todo = _context.ToDoItems.FirstOrDefault(x => x.Id == Id);
+        if (todo == null)
+        {
+            return BadRequest();
+        }
+        todo.Name = updateToDo.Name;
+        todo.Priority = updateToDo.Priority;
+        todo.DueDate = updateToDo.DueDate;
+        todo.DueTime = updateToDo.DueTime;
+        todo.Status = updateToDo.Status;
+
+        _context.ToDoItems.Update(todo);
+        _context.SaveChanges();
+
+        return Ok(todo);
+    }
+    [HttpDelete("delete")]
+    public IActionResult DeleteToDo([FromQuery] int Id)
+    {
+
+        var todo = _context.ToDoItems.FirstOrDefault(x => x.Id == Id);
+        if (todo == null)
+        {
+            return BadRequest();
+        }
+        _context.ToDoItems.Remove(todo);
+        _context.SaveChanges();
+        return Ok();
     }
 }
